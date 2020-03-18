00dc2a9b4710dd66200fba0e9755f26b08135f1d
schang1146 <sammychang2185@gmail.com>
2020-03-18 12:34:28 -0700

    finished miner.py but haven't tested yet

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,30 +4,32 @@ import requests
 import sys
 import json
 
-
-def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+from blockchain import Blockchain
 
 
-def valid_proof(block_string, proof):
-    """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
-    leading zeroes?  Return true if the proof is valid
-    :param block_string: <string> The stringified block to use to
-    check in combination with `proof`
-    :param proof: <int?> The value that when combined with the
-    stringified previous block results in a hash that has the
-    correct number of leading zeroes.
-    :return: True if the resulting hash is a valid proof, False otherwise
-    """
-    pass
+# def proof_of_work(block):
+#     """
+#     Simple Proof of Work Algorithm
+#     Stringify the block and look for a proof.
+#     Loop through possibilities, checking each one against `valid_proof`
+#     in an effort to find a number that is a valid proof
+#     :return: A valid proof for the provided block
+#     """
+#     pass
+
+
+# def valid_proof(block_string, proof):
+#     """
+#     Validates the Proof:  Does hash(block_string, proof) contain 6
+#     leading zeroes?  Return true if the proof is valid
+#     :param block_string: <string> The stringified block to use to
+#     check in combination with `proof`
+#     :param proof: <int?> The value that when combined with the
+#     stringified previous block results in a hash that has the
+#     correct number of leading zeroes.
+#     :return: True if the resulting hash is a valid proof, False otherwise
+#     """
+#     pass
 
 
 if __name__ == '__main__':
@@ -49,6 +51,7 @@ if __name__ == '__main__':
         # Handle non-json response
         try:
             data = r.json()
+            print(data)
         except ValueError:
             print("Error:  Non-json response")
             print("Response returned:")
@@ -56,7 +59,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = Blockchain.proof_of_work(data)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +70,4 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        print(data.message)
